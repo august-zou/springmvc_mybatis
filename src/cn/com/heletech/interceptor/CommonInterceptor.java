8ff806c001665d233b00c9d431350513f4a10335
august-zou <zjzouxu@gmail.com>
2015-05-19 14:55:23 +0800

    add all

---
+++ src/cn/com/heletech/interceptor/CommonInterceptor.java
@@ -0,0 +1,24 @@
+package cn.com.heletech.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+public class CommonInterceptor extends HandlerInterceptorAdapter {
+
+	  @Override  
+	    public boolean preHandle(HttpServletRequest request,  
+	            HttpServletResponse response, Object handler) throws Exception { 
+		  return true;
+		  
+	  }
+	  
+	  @Override  
+	    public void postHandle(HttpServletRequest request,  
+	            HttpServletResponse response, Object handler,  
+	            ModelAndView modelAndView) throws Exception {
+	  
+	  }
+}
