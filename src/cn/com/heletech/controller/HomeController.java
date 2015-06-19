a539caaec309b9c5f735ec3b4eccd5ca42c74356
august-zou <zjzouxu@gmail.com>
2015-06-19 10:51:06 +0800

    add log

--- src/cn/com/heletech/controller/HomeController.java
+++ src/cn/com/heletech/controller/HomeController.java
@@ -1,5 +1,7 @@
 package cn.com.heletech.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,12 +9,15 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HomeController extends ApplicationController{
+	private final Logger log = LoggerFactory.getLogger(HomeController.class);
+
 	/**
 	 * index
 	 * @return
 	 */
 	@RequestMapping(value = "/",method = RequestMethod.GET)
     public String index(Model model) {
+		log.info("home controller exe");
         return "index";
     }
 	
